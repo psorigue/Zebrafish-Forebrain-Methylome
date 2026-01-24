c62838878376986e3b1f785c1532fc0d09375251
Pol Sorigue <108137110+psorigue@users.noreply.github.com>
2026-01-24 02:29:27 +0000

    s

--- Scripts/04.04.3.PCA_forebrain_vs_whole-brain.R
+++ Scripts/04.04.3.PCA_forebrain_vs_whole-brain.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(dplyr)
 
-regions_name <- "genome_50kb_bins"
+regions_name <- "promoters"
 
 # 1. Create dataset with whole-brain RRBS
 
@@ -19,11 +19,15 @@ for (sam in samples_wb) {
   # Remove regions without calls
   ds <- ds[ds$V7 != 0, ]
   
-  # Reduce dataset. 4th column is methylation percentage
-  ds_red <- ds[, 1:4]
+  # Reduce dataset. 5th column is methylation percentage
+  ds_red <- ds[, c(1:3,5)]
+  
+  # Percentage over 1
+  ds_red$V5 <- as.numeric(ds_red$V5)
+  ds_red$V5 <- ds_red$V5 / 100 
   
   # Include sample name in % column
-  colnames(ds_red) <- c("chr", "start", "end", paste0(sam, "_wb_pct"))
+  colnames(ds_red) <- c("chr", "start", "end", paste0(sam, "_wb_mpct"))
   
   # Merge into dataset
   if (is.null(ds_wb)) {
@@ -37,43 +41,26 @@ for (sam in samples_wb) {
 
 
 # 2. Create dataset with forebrain ONT 
-# Because we compare to RRBS, we must add up the signal that 5mC and 5hmC produced in nanopore. RRBS signal includes 5mC 
-# and 5hmC modifications
 
 samples_fb <- c("02", "03", "04", "05")
-path_fb <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/", regions_name)
+path_fb <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/Chaterjee/forebrain_datasets/methylation_regions/", regions_name)
 
 ds_fb <- NULL   # initialize
 
 for (sam in samples_fb) {
-  #sam <- "01"
-  # Read files
-  file_m <- file.path(path_fb, paste0("5mC/meth_mean_5mC_rep", sam, "_", regions_name, ".txt"))
-  file_h <- file.path(path_fb, paste0("5hmC/meth_mean_5hmC_rep", sam, "_", regions_name, ".txt"))
-  ds_m <- fread(file_m, sep = "\t", header = F)
-  ds_h <- fread(file_h, sep = "\t", header = F)
-  
-  # Concatenate files. We have to sum methylation calls, keeping coverage as it is
-  ds <- rbind(ds_m, ds_h)
-  
-  # Group same region and sum modification calls
-  ds_grouped <- ds %>%
-    group_by(V1, V2, V3) %>%
-    summarise(
-      V4 = sum(V4, na.rm = TRUE),
-      V7 = max(V7, na.rm = TRUE),
-      .groups = "drop"
-    ) %>%
-    select(V1, V2, V3, V4, V7)
+  #sam <- "02"
+  # Read file
+  file <- file.path(path_fb, paste0(sam, "_", regions_name, ".txt"))
+  ds <- read.csv(file, sep = "\t", header = F)
   
   # Remove regions without calls
-  ds_grouped <- ds_grouped[ds_grouped$V7 != 0, ]
+  ds <- ds[ds$V7 != 0, ]
   
-  # Reduce dataset. 4th column is methylation percentage
-  ds_red <- ds_grouped[, 1:4]
+  # Reduce dataset. 5th column is methylation percentage
+  ds_red <- ds[, c(1:3,5)]
   
   # Include sample name in % column
-  colnames(ds_red) <- c("chr", "start", "end", paste0(sam, "_fb_pct"))
+  colnames(ds_red) <- c("chr", "start", "end", paste0(sam, "_fb_mpct"))
   
   # Merge into dataset
   if (is.null(ds_fb)) {
@@ -84,3 +71,42 @@ for (sam in samples_fb) {
     ds_fb <- merge(ds_fb, ds_red, by = c("chr", "start", "end"), all = FALSE)
   }
 }
+
+
+# Join both large datasets
+final_ds <- merge(ds_wb, ds_fb, by = c("chr", "start", "end"))
+
+
+
+
+mat <- as.matrix(final_ds[, 4:ncol(final_ds)])
+storage.mode(mat) <- "numeric"
+
+pca <- prcomp(
+  t(mat),
+  center = TRUE,
+  scale. = F
+)
+
+plot(pca$x[,1], pca$x[,2],
+     xlab = "PC1",
+     ylab = "PC2",
+     pch = 19)
+
+
+library(ggplot2)
+
+pca_df <- data.frame(
+  PC1 = pca$x[,1],
+  PC2 = pca$x[,2],
+  sample = rownames(pca$x)
+)
+
+ggplot(pca_df, aes(PC1, PC2, label = sample)) +
+  geom_point(size = 3) +
+  geom_text(vjust = -0.6) +
+  theme_classic()
+
+
+
+
