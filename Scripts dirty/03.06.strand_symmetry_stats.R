861e1c2e35b314ce08e6ac4a3b19da446ce3875d
Pol Sorigue <108137110+psorigue@users.noreply.github.com>
2026-02-05 11:45:15 +0000

    s

--- Scripts dirty/03.06.strand_symmetry_stats.R
+++ Scripts dirty/03.06.strand_symmetry_stats.R
@@ -1,17 +1,14 @@
+library(dplyr)
+library(tidyr)
 
+
+# 1. Genomic bins
 name_analysis <- "genome_50kb_bins"
-
 file <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/output/", name_analysis, "/CH_", name_analysis, "_stranded.txt")
-
 ds <- read.csv(file, header = F, sep = "\t")
 
 # If genomic bins
 colnames(ds) <- c("chr", "start", "end", "name", "meth", "cov", "nCpG_cov", "replicate", "strand", "motif")
-# If promoters or gene bodies
-colnames(ds) <- c("chr", "start", "end", "name", "dum", "st_region", "meth", "cov", "nCpG_cov", "replicate", "strand", "motif")
-
-library(dplyr)
-library(tidyr)
 
 # Do the mean methylation level differ for each chromosome?
 chr_motif_results <- ds %>%
@@ -41,3 +38,37 @@ chr_motif_results <- chr_motif_results %>%
 # Write output
 out_file <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/", name_analysis, "_strand_stats.txt")
 write.table(chr_motif_results, out_file, sep ="\t", col.names = T, row.names = F,quote = F)
+
+
+# 2. Promoters and genes
+name_analysis <- "promoters"
+file <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/output/", name_analysis, "/CH_", name_analysis, "_stranded.txt")
+ds <- read.csv(file, header = F, sep = "\t")
+colnames(ds) <- c("chr", "start", "end", "name", "dum", "st_region", "meth", "cov", "nCpG_cov", "replicate", "strand", "motif")
+
+motif_results_all <- ds %>%
+  group_by(motif) %>%
+  summarise(
+    n_pos = sum(strand == "pos"),
+    n_neg = sum(strand == "neg"),
+    mean_pos = mean(meth[strand == "pos"], na.rm = TRUE),
+    mean_neg = mean(meth[strand == "neg"], na.rm = TRUE),
+    mean_diff = mean_pos - mean_neg,
+    p_value = t.test(
+      meth[strand == "pos"],
+      meth[strand == "neg"],
+      var.equal = FALSE
+    )$p.value,
+    .groups = "drop"
+  )
+
+motif_results_all
+
+# Correct FDR
+motif_results_all <- motif_results_all %>%
+  group_by(motif) %>%
+  mutate(padj = p.adjust(p_value, method = "BH")) %>%
+  ungroup()
+
+out_file <- paste0("//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/", name_analysis, "_strand_stats.txt")
+write.table(motif_results_all, out_file, sep ="\t", col.names = T, row.names = F,quote = F)
--- Scripts dirty/plots/plot02.02.1.mods_regions_cpg.R
+++ Scripts dirty/plots/plot02.02.1.mods_regions_cpg.R
@@ -1,22 +1,22 @@
-
-file <- "//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/plot/2B.region_mods/5mC_all.txt"
-
-ds <- read.csv(file, header = F, sep = "\t")
-
-colnames(ds) <- c("chr", "start", "end", "name", "meth", "cov", "nCpG_cov", "replicate", "type")
-
 library(ggplot2)
 
-p <- ggplot(ds, aes(x = type, y = meth)) +
-  geom_violin(trim = T, fill = "steelblue", alpha = 0.7) +
-  geom_boxplot(width = 0.1, outlier.shape = NA, alpha = 0.5) +
+file <- "//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/methylation_regions/output/cpg_all.txt"
+
+ds <- read.csv(file, header = T, sep = "\t")
+
+# Ensure 'modification' is a factor with the correct order
+ds$modification <- factor(ds$modification, levels = c("5mC", "5hmC"))
+# Use the same dodge width for violins and boxplots
+dodge_width <- 0.6
+
+p <- ggplot(ds, aes(x = type, y = meth, fill = modification)) +
+  geom_violin(trim = TRUE, alpha = 0.7, position = position_dodge(width = dodge_width)) +
+  geom_boxplot(width = 0.04, outlier.shape = NA, alpha = 0.5, position = position_dodge(width = dodge_width)) +
   labs(
     x = "Genomic feature type",
     y = "Methylation level",
-    title = "Methylation distribution by genomic type"
+    title = "Methylation distribution by genomic type and modification",
+    fill = "Modification"
   ) +
   theme_classic()
-
-
-out_file <- "//files1.igc.gulbenkian.pt/folders/ANB/Pol/Methylome/plot/2B.region_mods/5mC_all.pdf"
-ggsave(out_file, p, device = "pdf")
+p
